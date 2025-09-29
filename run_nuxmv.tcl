4ab0c15211b4bacb21bc50c7a8ace52918d23b36
Lyova Panosyan <panosyan.liiiov@gmail.com>
2025-09-29 20:42:35 +0300

    [SOURCE] add run_nuxmv.tcl

---
+++ run_nuxmv.tcl
@@ -0,0 +1,6 @@
+read_model -i "A-Verification-Approach-for-Composite-Smart-Contracts-Security-using-FSM/one smart contract/addsub/addsub.smv"
+flatten_hierarchy
+encode_variables
+build_model
+check_ctlspec
+quit
